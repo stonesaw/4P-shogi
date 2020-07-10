bec611eefdcc73b85c8098d9f4d580293ba0a9f1
stonesaw <ishichan1260@gmail.com>
2020-07-11 02:28:28 +0900

    Add Banmen Base

--- main.cpp
+++ main.cpp
@@ -1,33 +1,91 @@
 #include "DxLib.h"
 
+/*
+    4P-shogi : 4l«Šû
+
+    Author Sou, Fukasato, Chisato
+*/
+
+
+class Window {
+public:
+    int base_width = 640;
+    int base_height = 480;
+    double scale;
+    int width;
+    int height;
+
+    Window(double scale) {
+        this->scale = scale;
+        this->width =  (int)(base_width  * this->scale);
+        this->height = (int)(base_height * this->scale);
+    }
+};
+
+
+class Ban {
+public:
+    int ox, oy;
+    int width, height;
+    int masu_size;
+    int color;
+    int line_width;
+    
+    Ban(Window window, int masu_size, unsigned int color, int line_width) {
+        this->masu_size = masu_size * window.scale;
+        this->width = this->masu_size * 9;
+        this->height = this->masu_size * 9;
+        this->ox = (window.width  - this->width) / 2;
+        this->oy = (window.height - this->height) / 2;
+        this->color = color;
+        this->line_width = line_width;
+    }
+
+    void Draw(Window window);
+};
+
+void Ban::Draw(Window window) {
+    DrawBox(this->ox, this->oy, this->ox + this->width, this->oy + this->height, this->color, TRUE);
+    for (int y = 0; y < 10; y++)
+        DrawBox(this->ox,                                                 this->oy + y * this->masu_size, 
+                this->ox + this->width + this->line_width * window.scale, this->oy + y * this->masu_size + this->line_width * window.scale, GetColor(0, 0, 0), TRUE);
+    for (int x = 0; x < 10; x++)
+        DrawBox(this->ox + x * this->masu_size,                                     this->oy, 
+                this->ox + x * this->masu_size + this->line_width * window.scale, this->oy + this->height,  GetColor(0, 0, 0), TRUE);
+}
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	int Cr;
-	double scale = 3;
-	SetGraphMode(640*scale, 480*scale, 32);
-	SetBackgroundColor(255, 255, 255);
-	ChangeWindowMode(TRUE);
+    int Cr;
 
+    Window window(3);
+    Ban ban(window, 50, GetColor(240, 181, 46), 2);
 
-	if (DxLib_Init() == -1) {
-		return -1;
-	}
+    SetGraphMode(window.width, window.height, 32);
+    SetBackgroundColor(255, 255, 255);
+    ChangeWindowMode(TRUE);
 
-	Cr = GetColor(255, 255, 255);
+    if (DxLib_Init() == -1) {
+        return -1;
+    }
 
-	while (1)
-	{
-		DrawBox(100, 100, 150, 150, Cr, FALSE);
-		if (ProcessMessage() == -1 || CheckHitKey(KEY_INPUT_ESCAPE)) {
-			DxLib_End();
-			return 0;
-		}
+    Cr = GetColor(255, 255, 255);
 
-		WaitTimer(10);
-	}
+    // main loop
+    while (1) {
+        if (ProcessMessage() == -1 || CheckHitKey(KEY_INPUT_ESCAPE)) {
+            DxLib_End();
+            return 0;
+        }
 
-	WaitKey();
+        ban.Draw(window);
 
-	DxLib_End();
+        WaitTimer(10);
+    }
 
-	return 0;
+    WaitKey();
+
+    DxLib_End();
+
+    return 0;
 }
